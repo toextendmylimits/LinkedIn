ee704ad53a2cf62a7dd6f1fe3f4f4d8005135fe1
toextendmylimits <bruce.yang.it@gmail.com>
2025-11-28 13:24:53 -0800

    Create top_20_questions_chatgpt.py

---
+++ top_20_questions_chatgpt.py
@@ -0,0 +1,41 @@
+On 2025-11-28
+
+(1) Two Sum
+
+(49) Group Anagrams
+
+(3) Longest Substring Without Repeating Characters
+
+(56) Merge Intervals
+
+(253) Meeting Rooms II
+
+(76) Minimum Window Substring
+
+(347) Top K Frequent Elements
+
+(206) Reverse Linked List
+
+(143) Reorder List
+
+(98) Validate BST
+
+(236) LCA of Binary Tree
+
+(230) Kth Smallest in BST
+
+(200) Number of Islands
+
+(207) Course Schedule
+
+(133) Clone Graph
+
+(198) House Robber
+
+(300) Longest Increasing Subsequence
+
+(153) Find Minimum in Rotated Sorted Array
+
+(23) Merge K Sorted Lists
+
+(271) Encode and Decode Strings
